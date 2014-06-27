b64279b741ea42da0ecc98e785d8ffdbf79a0290
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-06-27 17:19:10 +0200

    Add benchmark between Unicorn module and Hashtbl

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,12 @@
-module M = Rainbow.Poney(String)
-module U = Rainbow.Unicorn(String)
+open Core_bench.Std
+
+module Int = struct
+  type t = int
+
+  let compare = compare
+end
+
+module E = Rainbow.Unicorn(Int)
 
 let print_list ?(sep=", ") print_data out_ch lst =
   let rec aux = function
@@ -17,23 +24,45 @@ let print_int out_ch = Printf.fprintf out_ch "%d"
 let print_tuple print_a print_b out_ch (a, b) =
   Printf.fprintf out_ch "(%a, %a)" print_a a print_b b
 
-let () =
-  let a = U.create () in
+let rainbow_insert tbl number =
+  let rec aux = function
+    | 0 ->
+      E.add tbl 0 (Random.int 1000);
+    | n ->
+      E.add tbl n (Random.int 1000);
+      aux (n - 1)
+  in aux number
 
-  U.add a "toto" 1010;
-  U.add a "titi" 1111;
-  U.add a "tata" 1313;
-  U.add a "tutu" 1717;
+let hashtbl_insert tbl number =
+  let rec aux = function
+    | 0 ->
+      Hashtbl.add tbl 0 (Random.int 1000);
+    | n ->
+      Hashtbl.add tbl n (Random.int 1000);
+      aux (n - 1)
+  in aux number
 
-  U.iter (fun key value -> Printf.fprintf stderr "%s: %d\n%!" key value) a;
+let t1 =
+  ("Rainbow insert 1000",
+   (fun () -> rainbow_insert (E.create ()) 1000))
+let t2 =
+  ("Hashtbl[false,1] insert 1000",
+   (fun () -> hashtbl_insert (Hashtbl.create 1) 1000))
+let t3 =
+  ("Hashtbl[true,1] insert 1000",
+   (fun () -> hashtbl_insert (Hashtbl.create ~random:true 1) 1000))
+let t4 =
+  ("Hashtbl[false,512] insert 1000",
+   (fun () -> hashtbl_insert (Hashtbl.create 512) 1000))
+let t5 =
+  ("Hashtbl[true,512] insert 1000",
+   (fun () -> hashtbl_insert (Hashtbl.create ~random:true 512) 1000))
 
-  let l1 = U.fold
-      (fun key value acc ->
-         Gc.compact ();
-         (key, value) :: acc)
-      a []
-  in
+let benchmark () =
+  Core.Command.run
+    (Bench.make_command
+       (List.map
+          (fun (name, test) -> Bench.Test.create ~name test)
+          [t1; t2; t3; t4; t5]))
 
-  Printf.fprintf stderr "%a\n%!" (print_list (print_tuple print_string print_int)) l1;
-
-  ()
+let () = benchmark ()
